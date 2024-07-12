0daf7f1b4cb64aad7795f6b1083dd10da1358e47
Pierre Marquet <marquet_pierre@yahoo.fr>
2024-07-12 15:10:38 +0200

    add logo on career

--- src/components/pages/career/TimeLine.tsx
+++ src/components/pages/career/TimeLine.tsx
@@ -1,40 +1,58 @@
 "use client"
 
-import { ReactElement } from "react"
-import { MdOutlineLaptopMac, MdOutlineSchool } from "react-icons/md"
+import { ReactElement, ReactNode } from "react"
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
-
-import "react-vertical-timeline-component/style.min.css"
 
 import { career } from "@/config/career"
 
+import "react-vertical-timeline-component/style.min.css"
+
+const RenderTimelineContent = ({
+  title,
+  localisation,
+  description,
+  stackIcons,
+}: {
+  title: string
+  localisation: string
+  description?: ReactNode
+  stackIcons?: ReactElement[]
+}): ReactElement => (
+  <div className="flex flex-col space-y-3">
+    <strong className="text-lg font-bold md:text-2xl">{title}</strong>
+    <span className="italic md:text-lg">{localisation}</span>
+    {description && <span className="text-sm md:text-base">{description}</span>}
+    {stackIcons && (
+      <ul className="mt-5 flex flex-wrap items-center justify-center gap-2">
+        {stackIcons.map((icon, index) => (
+          <li key={index}>{icon}</li>
+        ))}
+      </ul>
+    )}
+  </div>
+)
+
 export default function TimeLine(): ReactElement {
   return (
-    <VerticalTimeline lineColor="gray">
+    <VerticalTimeline lineColor="gray" className="w-full">
       {career.map((item, index) => (
         <VerticalTimelineElement
           key={index}
           visible={process.env.NODE_ENV !== "production"}
           date={item.date}
-          dateClassName="dark:text-white text-black"
+          dateClassName="md:dark:text-white md:text-black text-white"
           contentStyle={item.type === "work" ? { background: "#8D27FF" } : { background: "#B40A74" }}
           textClassName="text-white bg-none"
-          icon={item.type === "work" ? <MdOutlineLaptopMac /> : <MdOutlineSchool />}
+          icon={item.logo}
           iconClassName={`${item.type === "work" ? "bg-primary" : "bg-secondary"} text-white`}
           contentArrowStyle={{ borderRight: item.type === "work" ? "10px solid #8D27FF" : "10px solid #B40A74" }}
         >
-          <div className="flex flex-col space-y-3">
-            <strong className="text-lg font-bold md:text-2xl">{item.title}</strong>
-            <span className="italic md:text-lg">{item.localisation}</span>
-            {item.description && <span className="text-sm md:text-base">{item.description}</span>}
-            {item.stackIcons ? (
-              <ul className="mt-5 flex flex-wrap items-center justify-center gap-2">
-                {item.stackIcons.map((icon, index) => (
-                  <li key={index}>{icon}</li>
-                ))}
-              </ul>
-            ) : null}
-          </div>
+          <RenderTimelineContent
+            title={item.title}
+            localisation={item.localisation}
+            description={item.description}
+            stackIcons={item.stackIcons}
+          />
         </VerticalTimelineElement>
       ))}
     </VerticalTimeline>
--- src/config/career.tsx
+++ src/config/career.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { ReactElement, ReactNode } from "react"
 import {
   BiLogoBootstrap,
@@ -17,12 +18,31 @@ import {
   BiLogoVuejs,
 } from "react-icons/bi"
 import { DiMysql, DiNodejsSmall, DiSymfonyBadge } from "react-icons/di"
+import { GiCrackedGlass } from "react-icons/gi"
 import { RiJavascriptFill, RiNextjsFill } from "react-icons/ri"
 import { SiAdobedreamweaver, SiAdobeillustrator, SiAdobeindesign, SiAdobephotoshop, SiCoreldraw } from "react-icons/si"
 import { TbBrandKotlin } from "react-icons/tb"
 
-interface careerInterface {
+import anfsi from "/public/images/career/anfsi.jpeg"
+import sdv from "/public/images/career/sdv.jpeg"
+import deletec from "/public/images/career/deletec.png"
+import tr from "/public/images/career/tr.png"
+import afpa from "/public/images/career/afpa.png"
+import cpmgn from "/public/images/career/cpmgn.png"
+import eg from "/public/images/career/eg.png"
+import scgd from "/public/images/career/scgd.jpg"
+import jfc from "/public/images/career/jfc.png"
+import creapub from "/public/images/career/creapub.jpg"
+import comg from "/public/images/career/comg.png"
+import studiom from "/public/images/career/studiom.png"
+import typoffset from "/public/images/career/typoffset.png"
+import lfm from "/public/images/career/lfm.png"
+import lrp from "/public/images/career/lrp.jpg"
+import vs from "/public/images/career/vs.jpg"
+
+export interface careerInterface {
   date: string
+  logo: ReactElement
   title: string
   localisation: string
   description?: ReactNode
@@ -36,6 +56,7 @@ const sizeReducer: number = 8
 export const career: careerInterface[] = [
   {
     date: "Octobre 2022 - Octobre 2024",
+    logo: <Image src={anfsi.src} alt="Logo ANFSI" width={200} height={200} className="rounded-full" />,
     title: "Apprenti Développeur Fullstack",
     localisation: "ANFSI - Issy-les-Moulineaux, Hauts-de-Seine (92)",
     description: (
@@ -73,6 +94,7 @@ export const career: careerInterface[] = [
   },
   {
     date: "Octobre 2022 - Octobre 2024",
+    logo: <Image src={sdv.src} alt="Logo Sup de Vinci" width={200} height={200} className="rounded-full" />,
     title: "Master Expert des Systèmes d’Information - Spécialisation Développement",
     localisation: "Sup de vinci - La Défense, Hauts-de-Seine (92)",
     type: "education",
@@ -92,6 +114,7 @@ export const career: careerInterface[] = [
   },
   {
     date: "Novembre 2021 - Octobre 2022",
+    logo: <Image src={deletec.src} alt="Logo DELETEC" width={200} height={200} className="rounded-full" />,
     title: "Apprenti Développeur Web / Chef de projet",
     localisation: "DELETEC - Paris (75)",
     description: (
@@ -131,6 +154,7 @@ export const career: careerInterface[] = [
   },
   {
     date: "Septembre 2021 - Septembre 2022",
+    logo: <Image src={sdv.src} alt="Logo Sup de Vinci" width={200} height={200} className="rounded-full" />,
     title: "Bachelor 3 Administrateur des Systèmes d'Information - Spécialisation Développement",
     localisation: "Sup de vinci - La Défense, Hauts-de-Seine (92)",
     type: "education",
@@ -148,6 +172,7 @@ export const career: careerInterface[] = [
   },
   {
     date: "Mai - Juillet 2020",
+    logo: <Image src={tr.src} alt="Logo la Team Rocket" width={200} height={200} className="rounded-full" />,
     title: "Stagiaire Développeur Web et web mobile / Chef de projet",
     localisation: "La Team Rocket - Toulouse, Haute-Garonne (31)",
     description: (
@@ -185,6 +210,7 @@ export const career: careerInterface[] = [
   },
   {
     date: "Novembre 2019 - Juillet 2020",
+    logo: <Image src={afpa.src} alt="Logo AFPA" width={200} height={200} className="rounded-full" />,
     title: "Titre professionnel Développeur Web et web mobile",
     localisation: "AFPA - Balma, Haute-Garonne (31)",
     type: "education",
@@ -200,6 +226,7 @@ export const career: careerInterface[] = [
   },
   {
     date: "Septembre 2014 - Septembre 2019",
+    logo: <Image src={cpmgn.src} alt="Logo CPMGN" width={200} height={200} className="rounded-full" />,
     title: "Gendarme Adjoint volontaire designer",
     localisation: "CPMGN - Limoges, Haute-Vienne (87)",
     description: (
@@ -233,12 +260,22 @@ export const career: careerInterface[] = [
   },
   {
     date: "Août 2014 - Septembre 2014",
+    logo: (
+      <Image
+        src={eg.src}
+        alt="Logo école de Gendarmerie de Montluçon"
+        width={200}
+        height={200}
+        className="rounded-full"
+      />
+    ),
     title: "Élève Gendarme Adjoint Volontaire",
     localisation: "École de Gendarmerie de Montluçon, Allier (03)",
     type: "education",
   },
   {
     date: "Mai - Juillet 2014",
+    logo: <Image src={scgd.src} alt="Logo Studio CG Designer" width={200} height={200} className="rounded-full" />,
     title: "Stagiaire designer / PAO",
     localisation: "Studio CG Designer - Narbonne, Aude (11)",
     description: (
@@ -267,6 +304,15 @@ export const career: careerInterface[] = [
   },
   {
     date: "Septembre 2013 - Juin 2014",
+    logo: (
+      <Image
+        src={jfc.src}
+        alt="Logo Lyçée Jean François Champollion"
+        width={200}
+        height={200}
+        className="rounded-full"
+      />
+    ),
     title: "1ʳᵉ année BTS Design d'espace et volume",
     localisation: "Lycee Jean François Champollion - Lattes, Hérault (34)",
     type: "education",
@@ -278,6 +324,7 @@ export const career: careerInterface[] = [
   },
   {
     date: "Avril - Mai 2013",
+    logo: <Image src={creapub.src} alt="Logo imprimerie Créapub" width={200} height={200} className="rounded-full" />,
     title: "Stagiaire designer et imprimeur Offset",
     localisation: "Créapub - Béziers, Hérault (34)",
     type: "work",
@@ -302,8 +349,9 @@ export const career: careerInterface[] = [
   },
   {
     date: "Mars - Avril 2013",
+    logo: <Image src={comg.src} alt="Logo Com Gaudry" width={200} height={200} className="rounded-full" />,
     title: "Stagiaire designer",
-    localisation: "Com.Gaudry - Sérignan, Hérault (34)",
+    localisation: "Com Gaudry - Sérignan, Hérault (34)",
     type: "work",
     description: (
       <ul className="list-inside list-disc">
@@ -329,6 +377,7 @@ export const career: careerInterface[] = [
   },
   {
     date: "Septembre 2013 - Juin 2014",
+    logo: <Image src={studiom.src} alt="Logo Studio M" width={200} height={200} className="rounded-full" />,
     title: "1ʳᵉ année Bachelor Art et Multimedia",
     localisation: "Studio M - Montpellier, Hérault (34)",
     type: "education",
@@ -344,6 +393,9 @@ export const career: careerInterface[] = [
   },
   {
     date: "Janvier - Mars 2012",
+    logo: (
+      <Image src={typoffset.src} alt="Logo imprimerie Typ'Offset" width={200} height={200} className="rounded-full" />
+    ),
     title: "Stagiaire designer",
     localisation: "Typ'offset - Béziers, Hérault (34)",
     type: "work",
@@ -359,11 +411,13 @@ export const career: careerInterface[] = [
     ),
     stackIcons: [
       <SiAdobeillustrator size={size - sizeReducer} key="ai" />,
-      <SiCoreldraw size={size - sizeReducer} key="cdr" />,
+      <SiAdobephotoshop size={size - sizeReducer} key="ps" />,
+      <SiAdobeindesign size={size - sizeReducer} key="id" />,
     ],
   },
   {
     date: "Mai - Juillet 2011",
+    logo: <GiCrackedGlass className="bg-white text-primary" />,
     title: "Stagiaire designer de vitrail",
     localisation: "Atelier de vitrail d’art - Béziers, Hérault (34)",
     type: "work",
@@ -379,12 +433,12 @@ export const career: careerInterface[] = [
     ),
     stackIcons: [
       <SiAdobeillustrator size={size - sizeReducer} key="ai" />,
-      <SiAdobephotoshop size={size - sizeReducer} key="ps" />,
-      <SiAdobeindesign size={size - sizeReducer} key="id" />,
+      <SiCoreldraw size={size - sizeReducer} key="cdr" />,
     ],
   },
   {
     date: "Septembre 2010 - Juillet 2012",
+    logo: <Image src={lfm.src} alt="Logo lyçée Frédéric Mistral" width={200} height={200} className="rounded-full" />,
     title: "Brevet des métiers d'art - Option A graphiste en lettres et décor",
     localisation: "Lycee Frédéric Mistral - Nîmes, Gard (30)",
     type: "education",
@@ -392,6 +446,63 @@ export const career: careerInterface[] = [
       <SiAdobeillustrator size={size - sizeReducer} key="ai" />,
       <SiAdobephotoshop size={size - sizeReducer} key="ps" />,
       <SiAdobeindesign size={size - sizeReducer} key="id" />,
+    ],
+  },
+  {
+    date: "Janvier - Février 2010",
+    logo: (
+      <Image
+        src={lrp.src}
+        alt="Logo Languedoc-roussillon publicité"
+        width={200}
+        height={200}
+        className="rounded-full"
+      />
+    ),
+    title: "Stagiaire sérigraphe",
+    localisation: "Languedoc-roussillon publicité - Sauvian, Hérault (34)",
+    type: "work",
+    description: (
+      <ul className="list-inside list-disc">
+        <li>
+          <strong>Création graphique</strong> (<strong>affiche</strong>, <strong>flyers</strong>, ...)
+        </li>
+        <li>Impression en sérigraphique</li>
+      </ul>
+    ),
+    stackIcons: [
+      <SiAdobeillustrator size={size - sizeReducer} key="ai" />,
+      <SiAdobephotoshop size={size - sizeReducer} key="ps" />,
+    ],
+  },
+  {
+    date: "Mai - Juin 2009",
+    logo: <Image src={vs.src} alt="Logo Vilar sérigraphie" width={200} height={200} className="rounded-full" />,
+    title: "Stagiaire sérigraphe",
+    localisation: "Vilar sérigraphie - Vendres, Hérault (34)",
+    type: "work",
+    description: (
+      <ul className="list-inside list-disc">
+        <li>
+          <strong>Création graphique</strong> (<strong>affiche</strong>, <strong>flyers</strong>, ...)
+        </li>
+        <li>Impression en sérigraphique</li>
+      </ul>
+    ),
+    stackIcons: [
+      <SiAdobeillustrator size={size - sizeReducer} key="ai" />,
+      <SiAdobephotoshop size={size - sizeReducer} key="ps" />,
+    ],
+  },
+  {
+    date: "Septembre 2008 - Juillet 2010",
+    logo: <Image src={lfm.src} alt="Logo lyçée Frédéric Mistral" width={200} height={200} className="rounded-full" />,
+    title: "CAP Sérigraphie industrielle",
+    localisation: "Lycee Frédéric Mistral - Nîmes, Gard (30)",
+    type: "education",
+    stackIcons: [
+      <SiAdobeillustrator size={size - sizeReducer} key="ai" />,
+      <SiAdobephotoshop size={size - sizeReducer} key="ps" />,
     ],
   },
 ]
