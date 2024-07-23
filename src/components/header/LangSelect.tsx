09502c37eaf260efc02edcebf3758d7bcf1fd39e
Pierre Marquet <marquet_pierre@yahoo.fr>
2024-07-23 11:32:22 +0200

    fix accessibility on lang select

--- src/components/header/LangSelect.tsx
+++ src/components/header/LangSelect.tsx
@@ -38,7 +38,7 @@ export default function LangSelect(): ReactElement {
         onChange={onSelectChange}
         defaultValue={localActive}
         disabled={isPending}
-        className="h-10 cursor-pointer rounded-md border border-input bg-background pl-2 text-sm ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        className="h-10 cursor-pointer rounded-md border border-input bg-background px-2 text-sm ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
       >
         {langs.map((lang, index) => {
           return (
