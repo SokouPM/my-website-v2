e9a3848cd2a817dfc1ef0a5d10c2b5845a94b78f
Pierre Marquet <marquet_pierre@yahoo.fr>
2024-08-20 09:12:46 +0200

    Remove projects page link

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -9,19 +9,19 @@ export const menu: MenuItemInterface[] = [
     name: "about-me.name",
     href: "/about-me",
   },
-  {
-    name: "projects.name",
-    children: [
-      {
-        name: "projects.children.design-projects",
-        href: "/design-projects",
-      },
-      {
-        name: "projects.children.web-projects",
-        href: "/web-projects",
-      },
-    ],
-  },
+  // {
+  //   name: "projects.name",
+  //   children: [
+  //     {
+  //       name: "projects.children.design-projects",
+  //       href: "/design-projects",
+  //     },
+  //     {
+  //       name: "projects.children.web-projects",
+  //       href: "/web-projects",
+  //     },
+  //   ],
+  // },
   {
     name: "career.name",
     href: "/career",
