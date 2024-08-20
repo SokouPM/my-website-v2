9aab428c117868285912a7b2e70c246f93d77eeb
Pierre Marquet <marquet_pierre@yahoo.fr>
2024-08-20 09:15:29 +0200

    Fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import createNextIntlPlugin from "next-intl/plugin"
 
 const withNextIntl = createNextIntlPlugin()
@@ -5,16 +6,17 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "standalone",
-  webpack: (config, options) => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.(pdf)$/,
-      type: "asset/resource"
+      type: "asset/resource",
     })
+
     return config
   },
   images: {
-    domains: ["dummyimage.com", "api.microlink.io"]
-  }
+    remotePatterns: [{ hostname: "api.microlink.io" }, { hostname: "dummyimage.com" }],
+  },
 }
 
 export default withNextIntl(nextConfig)
