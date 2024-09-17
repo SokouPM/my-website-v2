4d1005e5cdd5ff01619877fdf2aaadf629c1cba1
Pierre Marquet <marquet_pierre@yahoo.fr>
2024-09-17 17:24:42 +0200

    change last exp cards

--- src/components/body/aboutMe/LastExpCards.tsx
+++ src/components/body/aboutMe/LastExpCards.tsx
@@ -8,46 +8,51 @@ import { CareerInterface } from "@/interfaces/career"
 export default function LastExpCards() {
   const t = useTranslations("pages.career")
 
-  const lastJob: CareerInterface = Career.filter((job) => job.type === "work")[0]
-  const lastInternship: CareerInterface = Career.filter((job) => job.type === "education")[0]
+  const lastJob: CareerInterface = Career.filter((job): boolean => job.type === "work")[0]
+  const lastInternship: CareerInterface = Career.filter((job): boolean => job.type === "education")[0]
   const imgSize = 75
-  const _iconSize = 60
+
+  if (!lastJob && !lastInternship) return null
 
   return (
     <div className="mx-auto mb-5 flex flex-col gap-3 md:flex-row lg:w-11/12">
-      <Card className="flex w-full flex-col gap-2 p-3 md:w-1/2">
-        <CardHeader className="flex items-center justify-center justify-between gap-2 border-b pb-3">
-          {lastJob.logo?.src && (
-            <Image
-              src={lastJob.logo?.src}
-              alt={t(lastJob.logo?.alt)}
-              width={imgSize}
-              height={imgSize}
-              className="other-diagonal-sides"
-            />
-          )}
-          <CardTitle className="w-full font-bold md:text-lg">{t(lastJob.title)}</CardTitle>
-        </CardHeader>
-        <CardContent className="md:text-md">{t(lastJob.localisation)}</CardContent>
-        <CardFooter className="italic md:text-sm">{t(lastJob.date)}</CardFooter>
-      </Card>
+      {lastJob && (
+        <Card className="other-diagonal-sides flex w-full flex-col gap-2 border-none bg-gray-300 p-3 md:w-1/2 dark:bg-gray-800">
+          <CardHeader className="flex items-center gap-2 border-b pb-3">
+            {lastJob.logo?.src && (
+              <Image
+                src={lastJob.logo?.src}
+                alt={t(lastJob.logo?.alt)}
+                width={imgSize}
+                height={imgSize}
+                className="other-diagonal-sides"
+              />
+            )}
+            <CardTitle className="w-full font-bold md:text-lg">{t(lastJob.title)}</CardTitle>
+          </CardHeader>
+          <CardContent className="md:text-md">{t(lastJob.localisation)}</CardContent>
+          <CardFooter className="italic md:text-sm">{t(lastJob.date)}</CardFooter>
+        </Card>
+      )}
 
-      <Card className="flex w-full flex-col gap-2 p-3 md:w-1/2">
-        <CardHeader className="flex items-center justify-center justify-between gap-2 border-b pb-3">
-          {lastInternship.logo?.src && (
-            <Image
-              src={lastInternship.logo?.src}
-              alt={t(lastInternship.logo?.alt)}
-              width={imgSize}
-              height={imgSize}
-              className="other-diagonal-sides"
-            />
-          )}
-          <CardTitle className="w-full font-bold md:text-lg">{t(lastInternship.title)}</CardTitle>
-        </CardHeader>
-        <CardContent className="md:text-md">{t(lastInternship.localisation)}</CardContent>
-        <CardFooter className="italic md:text-sm">{t(lastInternship.date)}</CardFooter>
-      </Card>
+      {lastInternship && (
+        <Card className="other-diagonal-sides flex w-full flex-col gap-2 border-none bg-gray-300 p-3 md:w-1/2 dark:bg-gray-800">
+          <CardHeader className="flex items-center gap-2 border-b pb-3">
+            {lastInternship.logo?.src && (
+              <Image
+                src={lastInternship.logo?.src}
+                alt={t(lastInternship.logo?.alt)}
+                width={imgSize}
+                height={imgSize}
+                className="other-diagonal-sides"
+              />
+            )}
+            <CardTitle className="w-full font-bold md:text-lg">{t(lastInternship.title)}</CardTitle>
+          </CardHeader>
+          <CardContent className="md:text-md">{t(lastInternship.localisation)}</CardContent>
+          <CardFooter className="italic md:text-sm">{t(lastInternship.date)}</CardFooter>
+        </Card>
+      )}
     </div>
   )
 }
