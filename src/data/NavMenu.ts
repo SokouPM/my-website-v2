73763f14d6c19a8bd640527a4e15b91f53add66f
Pierre Marquet <marquet_pierre@yahoo.fr>
2025-02-26 15:13:12 +0100

    Modification navigation projets

--- src/data/NavMenu.ts
+++ src/data/NavMenu.ts
@@ -6,17 +6,17 @@ const navMenu: NavMenuItemInterface[] = [
     href: "/about-me",
   },
   {
-    name: "projects.name",
-    children: [
+    // name: "projects.name",
+    // children: [
       // {
       //   name: "projects.children.design-projects",
       //   href: "/design-projects",
       // },
-      {
-        name: "projects.children.web-projects",
-        href: "/web-projects",
-      },
-    ],
+      // {
+  name: "projects.children.web-projects",
+  href: "/web-projects",
+      // },
+    // ],
   },
   {
     name: "career.name",
